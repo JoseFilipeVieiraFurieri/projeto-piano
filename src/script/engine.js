c658a75b194f029e17b6b33f30efe87795e479f4
José Filipe Vieira Furieri <jf.furieri@ymail.com>
2024-01-24 01:18:36 -0300

    concluido eventos de keydown, audio e outros acertos

--- src/script/engine.js
+++ src/script/engine.js
@@ -6,7 +6,16 @@ const playTune = (key) => {
     const audio = new Audio(`src/tunes/${key}.mp3`);
     audio.volume = 0.8;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+    clickedKey.classList.add("active")
+    setTimeout(() => {
+      clickedKey.classList.remove("active")
+    }, 150)
 };
+
+// quando a keytune e acionada atraves do keydown ele adiciona a classe active que adiciona o mesmo efeito de sombreamento dando para o clique( porque agora é uma classe e não um prop)
+// com o settimeout ele remove a classe para evitar que o efeito de sombreamento fique por muito tempo
 
 
 pianoKeys.forEach((key) => {
@@ -17,8 +26,7 @@ pianoKeys.forEach((key) => {
 
 document.addEventListener("keydown", (e) => {
     console.log(e.key);
-    const audio = new Audio(`src/tunes/${e.key}.mp3`);
-    audio.play();
+    playTune(e.key)
 })
 
 
