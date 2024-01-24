5893394854f83bbb602b076bfe13de913ea0e4ad
José Filipe Vieira Furieri <jf.furieri@ymail.com>
2024-01-24 00:24:44 -0300

    volume teste

--- src/script/engine.js
+++ src/script/engine.js
@@ -4,7 +4,7 @@ const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
 const playTune = (key) => {
     const audio = new Audio(`src/tunes/${key}.mp3`);
-    audio.volume = 1.5;
+    audio.volume = 1;
     audio.play();
 };
 
